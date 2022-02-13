5bd6b0342d212940ade8bc6ac2ab0559cdefda20
pimoux <luka.vegeta@laposte.net>
2022-02-13 15:54:40 +0100

    animation improved a little bit

---
+++ Library/Components/LoadingCircles.swift
@@ -0,0 +1,57 @@
+//
+//  LoadingCircles.swift
+//  Library
+//
+//  Created by LUKA Vouillemont on 13/02/2022.
+//
+
+import SwiftUI
+
+struct LoadingCircles: View {
+    var shouldAnimate: Bool
+    var body: some View {
+        HStack {
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 20, height: 20)
+                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                .animation(shouldAnimate ?
+                           Animation
+                            .easeInOut(duration: 0.5)
+                            .repeatForever():
+                            Animation.default,
+                           value: shouldAnimate
+                )
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 20, height: 20)
+                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                .animation(shouldAnimate ?
+                           Animation
+                            .easeInOut(duration: 0.5)
+                            .repeatForever()
+                            .delay(0.3):
+                            Animation.default,
+                           value: shouldAnimate
+                )
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 20, height: 20)
+                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                .animation(shouldAnimate ?
+                           Animation
+                            .easeInOut(duration: 0.5)
+                            .repeatForever()
+                            .delay(0.6):
+                            Animation.default,
+                           value: shouldAnimate
+                )
+        }
+    }
+}
+
+struct LoadingCircles_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingCircles(shouldAnimate: false)
+    }
+}
--- Library/Test.swift
+++ Library/Test.swift
@@ -24,23 +24,7 @@ struct Test: View {
                 }
             } label: {
                 if displayLoading {
-                    HStack {
-                        Circle()
-                            .fill(Color.blue)
-                            .frame(width: 20, height: 20)
-                            .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                            .animation(Animation.easeInOut(duration: 0.5).repeatForever(), value: shouldAnimate)
-                        Circle()
-                            .fill(Color.blue)
-                            .frame(width: 20, height: 20)
-                            .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                            .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3), value: shouldAnimate)
-                        Circle()
-                            .fill(Color.blue)
-                            .frame(width: 20, height: 20)
-                            .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                            .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6), value: shouldAnimate)
-                    }
+                    LoadingCircles(shouldAnimate: shouldAnimate)
                 } else {
                     Text("Animate")
                 }
